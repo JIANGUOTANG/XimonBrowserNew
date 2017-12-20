a6de7d89b87fec65085859954d9d309f5a0a68a2
JianGuotang <zhxmGuotang>
2017-12-20 14:08:06 +0800

    更新了这个充电桩的画面，自定义了充电桩的显示和动画

--- ximon/src/main/java/jian/com/ximon/light/LightActivity.java
+++ ximon/src/main/java/jian/com/ximon/light/LightActivity.java
@@ -11,6 +11,7 @@ import jian.com.ximon.R;
  */
 
 public class LightActivity extends BaseActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
